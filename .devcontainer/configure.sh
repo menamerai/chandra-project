96c010a7be3de80d72aa9959fc34cc12a5f81dc3
menamerai <rphananh@gmail.com>
2025-04-06 02:02:29 -0400

    feat: cleanup

--- .devcontainer/configure.sh
+++ .devcontainer/configure.sh
@@ -4,8 +4,5 @@ sudo apt-get update
 sudo apt-get install -y ffmpeg
 sudo apt install build-essential gcc cmake wget python3-pip -y
 
-# Install ROS 2 dependencies
-bash ./.devcontainer/install_ros2_dependencies.sh
-
 git config --global user.email "${GIT_USER_EMAIL}"
 git config --global user.name "${GIT_USER_NAME}"
